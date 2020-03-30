08fa9e824c9dd949ae8d16f27519c43e3e3d9bfb
diego <diego@pop-os.localdomain>
2020-03-30 18:43:39 -0300

    implementing celestial tests

---
+++ tests/unit/celestial.spec.js
@@ -0,0 +1,8 @@
+import { shallowMount } from '@vue/test-utils'
+import Vue from 'vue'
+import Celestial from '@/lib-components/celestial.vue'
+import '@babel/polyfill'
+
+describe('celestial.vue', () => {
+
+})
