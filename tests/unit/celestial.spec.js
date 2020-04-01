55753b0df1bab6a03f9911e24cb24d4980a4bc4d
diego <diego@pop-os.localdomain>
2020-04-01 10:25:39 -0300

    not implemented celestial tests

--- tests/unit/celestial.spec.js
+++
@@ -1,8 +0,0 @@
-// import { shallowMount } from '@vue/test-utils'
-// import Vue from 'vue'
-// import Celestial from '@/lib-components/celestial.vue'
-// import '@babel/polyfill'
-
-// describe('celestial.vue', () => {
-
-// })
