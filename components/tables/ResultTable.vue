b524c9e653eb888f428b7465f6af7c8e5caf8149
Diego <diego@pop-os.localdomain>
2020-07-24 22:12:54 -0400

    Added slots and ordering results

--- components/tables/ResultTable.vue
+++ components/tables/ResultTable.vue
@@ -1,90 +1,88 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="8">
-        <v-flex xs6 md6>
-          <v-subheader>{{ message }}</v-subheader>
-        </v-flex>
-        <v-flex xs6 md6>
-          <!-- <columns-options-button :columns="selectedColumnOptions" /> -->
-        </v-flex>
-      </v-col>
-
-      <v-col cols="4">
-        <v-pagination
-          v-show="$vuetify.breakpoint.mdAndUp"
-          v-model="pageSync"
-          :length="numPages"
-          :total-visible="5"
-        />
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-data-table
-        :headers="header"
-        :items="tableItems"
-        :items-per-page="perPage"
-        :loading="loading"
-        hide-default-footer
-        dense
-        @click:row="onRowClicked"
-      />
-    </v-row>
-  </v-container>
+  <v-data-table
+    :headers="header"
+    :items="items"
+    :loading="loading"
+    :no-data-text="localMessage"
+    :server-items-length="total"
+    :options.sync="localPaginationOptions"
+    hide-default-footer
+    dense
+    @update:page="onPageChange"
+    @update:sort-by="onSortByChange"
+    @update:sort-desc="onSortByChange"
+    @update:options="onOptionsChange"
+    @click:row="onRowClicked"
+  >
+    <template v-slot:top>
+      <slot name="top">
+        <v-toolbar v-show="items.length > 0" dense flat>
+          {{ paginationMessage }}
+          <v-spacer></v-spacer>
+          <v-pagination
+            v-show="$vuetify.breakpoint.mdAndUp"
+            v-model="localPaginationOptions.page"
+            :length="numPages"
+            :total-visible="8"
+          />
+        </v-toolbar>
+      </slot>
+    </template>
+  </v-data-table>
 </template>
 <script>
-import {
-  Vue,
-  Component,
-  Prop,
-  PropSync,
-  Emit,
-  Watch,
-} from 'nuxt-property-decorator'
+import { Vue, Component, Prop, Emit, Watch } from 'nuxt-property-decorator'
 @Component({})
 export default class ResultTable extends Vue {
-  @PropSync('page', { type: Number, default: 1 }) pageSync
-
-  @Prop('selected', { type: Object, default: () => {} }) selected
-
   @Prop({ type: Array, default: () => [] }) items
-
-  @Prop({ type: Number, default: 10 }) perPage
 
   @Prop({ type: Boolean, default: false }) loading
 
   @Prop({ type: Number, default: 0 }) total
 
-  message = 'Your results will appear here'
+  @Prop({ type: Array, default: () => [] }) columnOptions
+
+  @Prop({ type: Object, default: () => {} }) paginationOptions
+
+  @Prop({ type: String, default: 'Your results will appear here' })
+  defaultMessage
+
+  paginationMessage = ''
+
+  localMessage = ''
+
+  localPaginationOptions = {}
+
+  beforeMount() {
+    this.setLocalMessage()
+    this.setLocalPaginationOptions()
+    this.setPaginationMessage()
+  }
+
+  setPaginationMessage() {
+    if (this.items) {
+      this.onItemsChange(this.items)
+    }
+  }
+
+  setLocalMessage() {
+    if (this.items.length) {
+      this.localMessage = `Found ${this.total.toLocaleString()} results`
+    } else this.localMessage = this.defaultMessage
+  }
+
+  setLocalPaginationOptions() {
+    this.localPaginationOptions = this.paginationOptions
+  }
 
   get numPages() {
-    return Math.round(this.total / this.perPage) + 1
+    return Math.round(this.total / this.localPaginationOptions.itemsPerPage)
   }
 
   get header() {
-    return this.selectedColumnOptions.filter((x) => {
+    return this.columnOptions.filter((x) => {
       return x.show
     })
-  }
-
-  get tableItems() {
-    if (this.items && this.items.length) {
-      const keys = Object.keys(this.items[0])
-      return this.items.map((obj) => {
-        const objCopy = Object.assign({}, obj)
-        keys.forEach((key) => {
-          if (typeof obj[key] === 'number' && !Number.isInteger(obj[key])) {
-            objCopy[key] = objCopy[key].toFixed(3)
-          }
-        })
-        objCopy.radec =
-          objCopy.meanra && objCopy.meandec
-            ? `${objCopy.meanra},\t ${objCopy.meandec}`
-            : ''
-        return objCopy
-      })
-    }
-    return []
   }
 
   @Emit('rowClicked')
@@ -92,122 +90,29 @@ export default class ResultTable extends Vue {
     return item
   }
 
-  selectedColumnOptions = [
-    {
-      value: 'oid',
-      sortable: false,
-      text: 'Object ID',
-      show: true,
-    },
-    {
-      value: 'ndet',
-      sortable: true,
-      text: 'Number of detections',
-      show: true,
-    },
-    {
-      value: 'ndethist',
-      sortable: true,
-      text: 'Number of detections history',
-      show: false,
-    },
-    {
-      value: 'ncovhist',
-      sortable: true,
-      text: 'ncovhist',
-      show: false,
-    },
-    {
-      value: 'jdstarthist',
-      sortable: true,
-      text: 'jdstarthist',
-      show: false,
-    },
-    {
-      value: 'jdendhist',
-      sortable: true,
-      text: 'jdendhist',
-      show: false,
-    },
-    {
-      value: 'corrected',
-      sortable: true,
-      text: 'jdendhist',
-      show: false,
-    },
-    {
-      value: 'stellar',
-      sortable: true,
-      text: 'stellar',
-      show: false,
-    },
-    {
-      value: 'firstmjd',
-      text: 'FirstMJD',
-      sortable: true,
-      show: true,
-    },
-    {
-      value: 'lastmjd',
-      sortable: true,
-      text: 'LastMJD',
-      show: true,
-    },
-    {
-      value: 'radec',
-      text: 'RA/Dec (degrees)',
-      sortable: false,
-      show: true,
-    },
-    {
-      value: 'class',
-      sortable: false,
-      text: 'Highest Probability Class',
-      show: true,
-    },
-    {
-      value: 'probability',
-      sortable: true,
-      text: 'Highest Probability',
-      show: true,
-    },
-    {
-      value: 'deltamjd',
-      text: 'DeltaMJD (days)',
-      show: true,
-    },
-    {
-      value: 'meandec',
-      text: 'Dec (degrees)',
-      sortable: false,
-      show: false,
-    },
-    {
-      value: 'meanra',
-      text: 'RA (degrees)',
-      sortable: false,
-      show: false,
-    },
-    {
-      value: 'sigmara',
-      text: 'Sigma RA',
-      sortable: false,
-      show: false,
-    },
-    {
-      value: 'sigmadec',
-      text: 'Sigma DEC',
-      sortable: false,
-      show: false,
-    },
-  ]
-
   @Watch('items')
   onItemsChange(val) {
-    console.log('onItemsChange')
-    this.message = val.length
+    this.paginationMessage = val.length
       ? `Found ${this.total.toLocaleString()} results`
       : 'No results found'
+  }
+
+  @Emit('pageChange')
+  onPageChange(val) {
+    return val
+  }
+
+  @Emit('sortByChange')
+  onSortByChange() {
+    return {
+      sortBy: this.localPaginationOptions.sortBy,
+      sortDesc: this.localPaginationOptions.sortDesc,
+    }
+  }
+
+  @Emit('optionsChange')
+  onOptionsChange(val) {
+    return val
   }
 }
 </script>
