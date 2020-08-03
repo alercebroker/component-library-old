5e4c7eac8fce953d7e719171b520869685c07497
javier <javier.arredondo.c@usach.cl>
2020-08-03 17:06:05 -0400

    ADD: Download lightcurve (detections and non detections) in a zip

--- components/misc/SelectDisplay.vue
+++ components/misc/SelectDisplay.vue
@@ -6,7 +6,7 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-col>
+      <v-col cols="8">
         <v-radio-group v-model="selected" row>
           <div v-for="option in options" :key="'radio-' + option.value">
             <v-radio :value="option.value">
@@ -16,9 +16,9 @@
                   <span v-if="option.tooltip">
                     <v-tooltip bottom>
                       <template v-slot:activator="{ on, attrs }">
-                        <v-icon align="center" v-bind="attrs" v-on="on"
-                          >help</v-icon
-                        >
+                        <v-icon align="center" v-bind="attrs" v-on="on">
+                          help
+                        </v-icon>
                       </template>
                       <span>{{ option.tooltip }}</span>
                     </v-tooltip>
@@ -29,12 +29,19 @@
           </div>
         </v-radio-group>
       </v-col>
+      <v-col v-if="isDownloable" cols="4" align-self="center">
+        <v-btn @click="downloadLightcurve" outlined small>
+          <v-icon left small>cloud_download</v-icon>
+          Download
+        </v-btn>
+      </v-col>
     </v-row>
   </div>
 </template>
 
 <script>
 import { Vue, Component, Prop } from 'nuxt-property-decorator'
+import { download } from '../utils/DownloadData'
 @Component
 export default class SelectDisplay extends Vue {
   /**
@@ -48,6 +55,8 @@ export default class SelectDisplay extends Vue {
    * Optionally provide the default option true for starting with a value selected
    */
   @Prop({ type: Array, required: true }) options
+  @Prop({ type: Object }) lightcurve
+  @Prop({ type: String }) oid
 
   selected = null
 
@@ -57,6 +66,14 @@ export default class SelectDisplay extends Vue {
         return x.default
       }
     }).value
+  }
+
+  get isDownloable() {
+    return this.lightcurve !== undefined && this.oid != null
+  }
+
+  downloadLightcurve() {
+    download(this.oid, this.lightcurve)
   }
 }
 </script>
--- components/utils/DownloadData.js
+++ components/utils/DownloadData.js
@@ -1,30 +1,37 @@
-/* based in https://stackoverflow.com/questions/8847766/how-to-convert-json-to-csv-format-and-store-in-a-variable */
-function download(data) {
-  let arrayOfJson = data.detections
-  const replacer = (key, value) => (value === null ? "" : value); // specify how you want to handle null values here
-  const header = Object.keys(arrayOfJson[0])
-  let csv = arrayOfJson.map(row =>
-    header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(","))
-  csv.unshift(header.join(","))
-  csv = csv.join("\r\n")
-  // Create link and download
-  var link = document.createElement("a")
-  link.setAttribute(
-    "href",
-    "data:text/csv;charset=utf-8,%EF%BB%BF" +
-    encodeURIComponent(csv)
-  );
-  let today = new Date().toISOString()
-  let filename =
-    this.$store.state.results.selectedObject.oid +
-    "_" +
-    today +
-    ".csv";
-  link.setAttribute("download", filename)
-  link.style.visibility = "hidden"
-  document.body.appendChild(link)
-  link.click()
-  document.body.removeChild(link)
+import JSZip from 'jszip'
+import FileSaver from 'file-saver'
+
+function dateToString(date) {
+  date = date.toISOString()
+  date = date.replace(/[-:]/g, (c) => '')
+  // date = date.replace('T', '_')
+  date = date.split('T')[0]
+  return date
 }
 
-export default { download }
+function jsonToCsv(arrayOfJson) {
+  const replacer = (key, value) => (value === null ? '' : value)
+  const header = Object.keys(arrayOfJson[0])
+  let csv = arrayOfJson.map((row) =>
+    header
+      .map((fieldName) => JSON.stringify(row[fieldName], replacer))
+      .join(',')
+  )
+  csv.unshift(header.join(','))
+  csv = csv.join('\r\n')
+  return csv
+}
+
+/* based in https://stackoverflow.com/questions/8847766/how-to-convert-json-to-csv-format-and-store-in-a-variable */
+function download(oid, lightcurve) {
+  const today = dateToString(new Date())
+  const filename = `${oid}_${today}.zip`
+  const zip = new JSZip()
+  zip.file('detections.csv', jsonToCsv(lightcurve.detections))
+  zip.file('non_detections.csv', jsonToCsv(lightcurve.non_detections))
+  zip.generateAsync({ type: 'blob' }).then((content) => {
+    FileSaver.saveAs(content, filename)
+  })
+}
+
+export { download }
