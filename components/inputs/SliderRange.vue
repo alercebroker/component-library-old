be592e1ce81fdd04886b75fe6c7b3d13024facc2
Diego <diego@pop-os.localdomain>
2020-08-10 10:21:54 -0400

    fix slider ndet

--- components/inputs/SliderRange.vue
+++ components/inputs/SliderRange.vue
@@ -4,7 +4,13 @@
       <v-text-field v-model="minRange" type="number" label="min" :min="min" />
     </v-flex>
     <v-flex xs6 sm6 md6 pl-3 pr-3>
-      <v-range-slider v-model="range" :max="max" :min="min" :step="step" />
+      <v-range-slider
+        :value="value"
+        :max="max"
+        :min="min"
+        :step="step"
+        @end="setRange"
+      />
     </v-flex>
     <v-flex xs3 sm3 md3>
       <v-text-field v-model="maxRange" type="number" label="max" :max="max" />
@@ -16,7 +22,7 @@
 import { Vue, Component, Prop } from 'nuxt-property-decorator'
 @Component
 export default class SliderRange extends Vue {
-  @Prop({ type: Array, default: () => [0, 1000] }) value
+  @Prop({ type: Array, default: () => [1, 2000] }) value
 
   @Prop({ type: Number, default: null }) max
 
@@ -42,11 +48,7 @@ export default class SliderRange extends Vue {
     this.$emit('input', [minRange, val])
   }
 
-  get range() {
-    return this.value
-  }
-
-  set range(val) {
+  setRange(val) {
     this.$emit('input', val)
   }
 }
