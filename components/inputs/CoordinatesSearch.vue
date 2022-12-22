a65ac03d4de4dd6727dbae112656002636b9314d
Pedro Gallardo <pegallardo@inf.udec.cl>
2022-12-22 11:25:01 -0300

    style: eslint formatted

--- components/inputs/CoordinatesSearch.vue
+++ components/inputs/CoordinatesSearch.vue
@@ -4,67 +4,46 @@
     <v-flex xs10 sm12 md12>
       <v-row>
         <v-col lg="6" md="12">
-        <v-text-field
-          v-model="targetName"
-          label="Target Name"
-         :error-messages="error"
-          
-        />
+          <v-text-field v-model="targetName" label="Target Name" :error-messages="error" />
         </v-col>
-      <!--BOTON-->
-        <v-col lg="6" md="12" mt ="5">
-          <v-btn @click="obtenerInfo" :loading="isLoading" color="normal" block margin-top = "4" >
+        <!--BOTON-->
+        <v-col lg="6" md="12" mt="5">
+          <v-btn @click="obtenerInfo" :loading="isLoading" color="normal" block margin-top="4">
             Resolve
           </v-btn>
-        </v-col> 
+        </v-col>
       </v-row>
-    </v-flex> 
+    </v-flex>
     <!--PROVIDED BY-->
     <v-flex xs10 sm12 md12>
-    <v-row>
-      <v-col md="7" lg="12" class="pa-0 caption transparent py-0 ">
-        <p class="mb-0 mr-3 text-right">
-          Provided by
-          <a href="https://vizier.cds.unistra.fr/vizier/doc/sesame.htx" target="_blank">
-            Sesame
-          </a>
-        </p>
-      </v-col>
-    </v-row>
+      <v-row>
+        <v-col md="7" lg="12" class="pa-0 caption transparent py-0 ">
+          <p class="mb-0 mr-3 text-right">
+            Provided by
+            <a href="https://vizier.cds.unistra.fr/vizier/doc/sesame.htx" target="_blank">
+              Sesame
+            </a>
+          </p>
+        </v-col>
+      </v-row>
     </v-flex>
-    <!--RA--> 
-    <v-flex xs5>      
-      <v-text-field
-        v-model="localValue.ra"
-        type="number"
-        step="0.00001"
-        label="RA (deg)"
-        :error-messages="validationErrors.conesearch"
-      />
+    <!--RA-->
+    <v-flex xs5>
+      <v-text-field v-model="localValue.ra" type="number" step="0.00001" label="RA (deg)"
+        :error-messages="validationErrors.conesearch" />
     </v-flex>
     <v-spacer />
-    <!--DEC--> 
-    <v-flex xs5> 
-      <v-text-field
-        v-model="localValue.dec"
-        label="DEC (deg)"
-        type="number"
-        step="0.00001"
-        :error-messages="validationErrors.conesearch"
-      />
+    <!--DEC-->
+    <v-flex xs5>
+      <v-text-field v-model="localValue.dec" label="DEC (deg)" type="number" step="0.00001"
+        :error-messages="validationErrors.conesearch" />
     </v-flex>
-    <!--RADIUS-->      
-    <v-flex xs12> 
-      <v-text-field
-        v-model="localValue.radius"
-        label="Radius (arcsec)"
-        type="number"
-        step="0.00001"
-        min="0"
+    <!--RADIUS-->
+    <v-flex xs12>
+      <v-text-field v-model="localValue.radius" label="Radius (arcsec)" type="number" step="0.00001" min="0"
         :error-messages="
-          validationErrors.conesearch || validationErrors.radiusPositive
-        "
-      />
+  validationErrors.conesearch || validationErrors.radiusPositive
+        " />
     </v-flex>
   </v-layout>
 </template>
@@ -78,7 +57,7 @@ export default class CoordinatesSearch extends Vue {
    * Object with ra, dec and radius keys
    */
   @Prop({ type: Object, required: true }) value
-  @Prop({ type: Object, default: () => {} }) validationErrors
+  @Prop({ type: Object, default: () => { } }) validationErrors
 
   localValue = {}
   targetName = ""
@@ -99,11 +78,11 @@ export default class CoordinatesSearch extends Vue {
     this.isLoading = true;
     axios.get(`https://cds.unistra.fr/cgi-bin/nph-sesame/-ox?${this.targetName}`).then((result) => {
       let parser = new DOMParser();
-      let xmlDom = parser.parseFromString(result.data , 'application/xml');
+      let xmlDom = parser.parseFromString(result.data, 'application/xml');
       let jra = xmlDom.querySelector('jradeg')?.innerHTML;
       let jdec = xmlDom.querySelector('jdedeg')?.innerHTML;
 
-      if (!jra || !jdec){
+      if (!jra || !jdec) {
         this.error = ["Object not found"];
       }
 
@@ -111,8 +90,8 @@ export default class CoordinatesSearch extends Vue {
         this.localValue = {
           ...this.localValue,
           ra: +jra,
-          dec:+jdec,
-          radius:1.5
+          dec: +jdec,
+          radius: 1.5
         }
         this.error = []
       }
