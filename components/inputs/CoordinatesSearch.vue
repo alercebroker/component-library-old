d7fb5140cbc9880ae98454b263d61c05afdec87f
Pedro Gallardo <pegallardo@inf.udec.cl>
2023-01-19 12:53:24 -0300

    style: corrected function radectodegrees typo

--- components/inputs/CoordinatesSearch.vue
+++ components/inputs/CoordinatesSearch.vue
@@ -99,7 +99,7 @@ export default class CoordinatesSearch extends Vue {
   @Watch('localValue', { immediate: true, deep: true })
   onLocalValueChange(val) {
     if(this.usingHMS){
-      const raDec = this.raDectoDregrees(String(val.ra), String(val.dec))
+      const raDec = this.raDectoDegrees(String(val.ra), String(val.dec))
       this.$emit('input', {...val, ...raDec})
     }
     else{
@@ -130,7 +130,7 @@ export default class CoordinatesSearch extends Vue {
     }
   }
 
-  raDectoDregrees(hmsRa, hmsDec){
+  raDectoDegrees(hmsRa, hmsDec){
     const hmsR = hmsRa?.split(/:|\s+/).map(x => +x)
     const hmsD = hmsDec?.split(/:|\s+/).map(x => +x)
 
